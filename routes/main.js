b887e505428a4a5c4d6a3213536e9986df968879
xiaogit00 <leibingguo@gmail.com>
2022-01-31 20:39:49 +0800

    added sample dummy reponse data under /utils, as well as several routes to serve them

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,25 @@
+const nftEth = require('../util/nftEth.json')
+const nftAvax = require('../util/nftAvax.json')
+const nftPolygon = require('../util/nftPolygon.json')
 // this is where the routes for the app goes
 module.exports = function (app) {
-    
+
     // retrieve index page
     app.get("/", function (req, res) {
         res.render("index.html", {
             title: "hackathon: Road to web3"
         });
     });
+
+    app.get("/api/nft-eth", function (req, res) {
+        res.send(nftEth)
+    })
+
+    app.get("/api/nft-avax", function (req, res) {
+        res.send(nftAvax)
+    })
+
+    app.get("/api/nft-polygon", function (req, res) {
+        res.send(nftPolygon)
+    })
 }
